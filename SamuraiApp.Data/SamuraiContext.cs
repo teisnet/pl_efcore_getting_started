28baafad559ed9312e6e7b4de6c7b6c43cdca750
Teis Draiby <teis@teis.net>
2020-07-28 19:24:23 +0200

    master: 2.8 - Specify the data provider and the connection string

--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -8,5 +8,10 @@ namespace SamuraiApp.Data
 		public DbSet<Samurai> Samurais { get; set; }
 		public DbSet<Quote> Quotes { get; set; }
 		public DbSet<Clan> Clans { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = SamuraiAppData");
+		}
 	}
 }
