4ed2963c8c87b8381a8a83de4a34b55425ca0631
Teis Draiby <teis@teis.net>
2020-07-29 19:13:35 +0200

    Module 8 - Using EF Core with ASP.NET Core, after

--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -1,38 +1,23 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore.Logging;
 using SamuraiApp.Domain;
 
 namespace SamuraiApp.Data
 {
   public class SamuraiContext : DbContext
   {
-    public SamuraiContext()
+
+    public SamuraiContext(DbContextOptions<SamuraiContext> options)
+      :base(options)
     {
       ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
     }
+
     public DbSet<Samurai> Samurais { get; set; }
     public DbSet<Quote> Quotes { get; set; }
     public DbSet<Clan> Clans { get; set; }
     public DbSet<Battle> Battles { get; set; }
     public DbSet<SamuraiBattleStat> SamuraiBattleStats { get; set; }
-
-    public static readonly ILoggerFactory ConsoleLoggerFactory
-      = LoggerFactory.Create(builder =>
-      {
-        builder
-         .AddFilter((category, level) =>
-             category == DbLoggerCategory.Database.Command.Name
-             && level == LogLevel.Information)
-         .AddConsole();
-      });
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-      optionsBuilder
-         .UseLoggerFactory(ConsoleLoggerFactory)
-         .EnableSensitiveDataLogging()
-         .UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = SamuraiAppData");
-    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
