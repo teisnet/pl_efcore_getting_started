122721e6e1479e5340e79713abf94dd6eb1e2764
Teis Draiby <teis@teis.net>
2020-07-29 13:48:51 +0200

    Module 3 - Database Creation and Schema Changes, before

--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -1,9 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using SamuraiApp.Domain;
 
 namespace SamuraiApp.Data
 {
-  public class SamuraiContext : DbContext
+    public class SamuraiContext:DbContext
   {
     public DbSet<Samurai> Samurais { get; set; }
     public DbSet<Quote> Quotes { get; set; }
