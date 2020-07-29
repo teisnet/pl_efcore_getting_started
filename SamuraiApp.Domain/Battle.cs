9a723825b0bbc6e3a5e0bcbb069fa45cb06544b2
Teis Draiby <teis@teis.net>
2020-07-29 17:47:44 +0200

    Module 5 - Interacting with Your EF Core Data Model, after

--- SamuraiApp.Domain/Battle.cs
+++ SamuraiApp.Domain/Battle.cs
@@ -5,6 +5,10 @@ namespace SamuraiApp.Domain
 {
   public class Battle
   {
+    public Battle()
+    {
+      SamuraiBattles = new List<SamuraiBattle>();
+    }
     public int Id { get; set; }
     public string Name { get; set; }
     public DateTime StartDate { get; set;}
