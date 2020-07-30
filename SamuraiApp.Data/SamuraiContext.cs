4f9b51c626b105970f518434583bb84b7106b73b
Teis Draiby <teis@teis.net>
2020-07-31 00:12:41 +0200

    5.3 - Add logging to EF Core's workload

--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using SamuraiApp.Domain;
 
 namespace SamuraiApp.Data
@@ -12,9 +13,21 @@ namespace SamuraiApp.Data
 		// Though, in order to interact directly with the Battles, a DBSet is needed.
 		public DbSet<Battle> Battles { get; set; }
 
+
+		public static readonly ILoggerFactory ConsoleLoggerFactory = LoggerFactory.Create(builder => {
+			builder
+				.AddFilter((category, level) =>
+					category == DbLoggerCategory.Database.Command.Name
+					&& level == LogLevel.Information
+				)
+				.AddConsole();
+		});
+
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
-			optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = SamuraiAppData");
+			optionsBuilder
+				.UseLoggerFactory(ConsoleLoggerFactory)
+				.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = SamuraiAppData");
 		}
 
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
