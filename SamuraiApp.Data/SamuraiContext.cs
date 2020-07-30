a13ffc6cab76f834573fdd3016d4725c828b3cdf
Teis Draiby <teis@teis.net>
2020-07-30 19:00:23 +0200

    4.6 - Controlling table names with mappings

--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -8,6 +8,9 @@ namespace SamuraiApp.Data
 		public DbSet<Samurai> Samurais { get; set; }
 		public DbSet<Quote> Quotes { get; set; }
 		public DbSet<Clan> Clans { get; set; }
+		// The Battle database table will be created regardless the DBSet declaration, due to the relationships.
+		// Though, in order to interact directly with the Battles, a DBSet is needed.
+		public DbSet<Battle> Battles { get; set; }
 
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
@@ -18,6 +21,7 @@ namespace SamuraiApp.Data
 		{
 			// This tells EF Core that SamuraiBattle is the many-to-many joint table for the Samurai and Battle tables.
 			modelBuilder.Entity<SamuraiBattle>().HasKey(s => new { s.SamuraiId, s.BattleId });
+			modelBuilder.Entity<Horse>().ToTable("Horses");
 		}
 	}
 }
--- SamuraiApp.Domain/Horse.cs
+++ SamuraiApp.Domain/Horse.cs
@@ -7,6 +7,7 @@
 
 		// To make a one-to-one relationship, the foreign SamuraiId key
 		// along with the 'Horse' navigation property in the Samurai class is enough.
+		// For a one-to-one relationship, it would be possible to make EF Core merge the into the same table.
 		public int SamuraiId { get; set; }
 	}
 }
