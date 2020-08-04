3372b622381787ca30335dbad2250738096929af
Teis Draiby <teis@teis.net>
2020-08-04 18:31:41 +0200

    Refactor module 2 - Create your first app

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -22,12 +22,7 @@ namespace ConsoleApp
 		private static SamuraiContext context = new SamuraiContext();
 		static void Main(string[] args)
 		{
-			// context.Database.EnsureCreated();
-
 			#region Simple objects
-			// GetSamurais("Before add");
-			// AddSamurai();
-			// GetSamurais("After add");
 
 			// InsertMultipleSamurais();
 			// InstertVariousTypes();
@@ -580,24 +575,6 @@ namespace ConsoleApp
 			context.Samurais.AddRange(samurai, samurai2, samurai3, samurai4);
 
 			context.SaveChanges();
-		}
-
-		private static void AddSamurai()
-		{
-			var samurai = new Samurai{ Name = "Julie" };
-			context.Samurais.Add(samurai);
-			context.SaveChanges();
-		}
-
-		private static void GetSamurais(string text)
-		{
-			var samurais = context.Samurais.ToList();
-			Console.WriteLine($"{text}: Samurai count is {samurais.Count}");
-			foreach (var samurai in samurais)
-			{
-				Console.WriteLine(samurai.Name);
-			}
-
 		}
 
 		private static void GetSamuraisSimpler()
---
+++ ConsoleApp/Tutorial/Module2FirstApp.cs
@@ -0,0 +1,39 @@
+using SamuraiApp.Data;
+using SamuraiApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp.Tutorial
+{
+	public class Module2FirstApp
+	{
+		private static SamuraiContext context = new SamuraiContext();
+
+		public static void Run()
+		{
+			// context.Database.EnsureCreated();
+
+			// GetSamurais("Before add");
+			// AddSamurai();
+			// GetSamurais("After add");
+		}
+
+		private static void AddSamurai()
+		{
+			var samurai = new Samurai { Name = "Julie" };
+			context.Samurais.Add(samurai);
+			context.SaveChanges();
+		}
+
+		private static void GetSamurais(string text)
+		{
+			var samurais = context.Samurais.ToList();
+			Console.WriteLine($"{text}: Samurai count is {samurais.Count}");
+			foreach (var samurai in samurais)
+			{
+				Console.WriteLine(samurai.Name);
+			}
+		}
+	}
+}
