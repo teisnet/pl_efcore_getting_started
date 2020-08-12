c84ffd3480069d12738c396c7752bdda6529f2ee
Teis Draiby <teis@teis.net>
2020-08-13 01:14:07 +0200

    Module 9 - Console: Testing with the InMemory Provider Instead of a Real Database, before

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -13,10 +13,49 @@ namespace ConsoleApp
 
     private static void Main(string[] args)
     {
-      
+      //QuerySamuraiBattleStats();
+      //QueryUsingRawSql();
+      //QueryUsingRawSqlStoredProcParameters();
+      //InterpolatedQueryUsingRawSqlStoredProcParameters();
+      ExecuteSomeRawSql();
+     }
+
+    private static void QuerySamuraiBattleStats()
+    {
+      var stats = _context.SamuraiBattleStats.ToList();
+      var firstStat = _context.SamuraiBattleStats.FirstOrDefault();
+      var sampsonStat = _context.SamuraiBattleStats.Where(s => s.Name == "SampsonSan").FirstOrDefault();
+
+      _context.SamuraiBattleStats.Find(1);
+    } 
+    private static void QueryUsingRawSql()
+    {
+      var samurais = _context.Samurais.FromSqlRaw(
+        "Select Id, Name, ClanId from Samurais").Include(s=>s.Quotes).ToList();
+    }
+    private static void QueryUsingRawSqlStoredProcParameters()
+    {
+      var text = "Happy";
+      var samurais = _context.Samurais.FromSqlRaw(
+        "EXEC dbo.SamuraisWithFilteredQuotes {0} ", text).ToList();
+    }
+    private static void InterpolatedQueryUsingRawSqlStoredProcParameters()
+    {
+      var text = "Happy";
+      var samurais = _context.Samurais.FromSqlInterpolated(
+        $"EXEC dbo.SamuraisWithFilteredQuotes {text}").ToList();
     }
 
- 
+   private static void ExecuteSomeRawSql()
+    {
+      var samuraiId = 22;
+      //var x =_context.Database
+      //         .ExecuteSqlRaw("EXEC DeleteQuotesForSamurai {0}", samuraiId );
+      samuraiId = 31;
+       _context.Database
+             .ExecuteSqlInterpolated($"EXEC DeleteQuotesForSamurai {samuraiId}");
+
+    }
 
   }
 }
--- SamuraiApp.Domain/Horse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SamuraiApp.Domain
-{
-  public class Horse
-  {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public int SamuraiId { get; set; }
-  }
-}
